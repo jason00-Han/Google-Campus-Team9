059e9912e922d15015454674f1b4b6b0c94b6fd5
niceome <gylim7826@naver.com>
2025-11-04 18:07:14 +0900

    add new backend

---
+++ backend/src/main/java/com/team8/diary/DiaryServiceApplication.java
@@ -0,0 +1,20 @@
+package com.team8.diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaryServiceApplication.class, args);
+
+		System.out.println("Diary Service Application Started");
+		System.out.println("Hi on!");
+		System.out.println("try");
+
+		System.out.println("Hi spring");
+		System.out.println("main branch");
+	}
+
+}
